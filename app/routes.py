1227f9c291a70ebee838f9662bee227f4e9316d1
Mark Harmsen <mharmsenr@gmail.com>
2024-10-01 10:34:16 -0500

    adjust template to the tech wave mvp

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,20 @@ from app import app
 def index():
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'title': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'title': 'The Avengers movie was so cool!'
+        },
+        {
+            'title': 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+        },
+        {
+            'title': 'Instagram Implementa Restricciones para Proteger a Adolescentes en la Plataforma'
+        },
+        {
+            'title': 'Apple lanza el Watch Series 10 con detección de apnea de sueño y profundidad de agua, y el iPhone 16 se lanza sin su funcionalidad más esperada de IA'
         }
+
     ]
     return render_template('index.html', title='Home', posts=posts)
