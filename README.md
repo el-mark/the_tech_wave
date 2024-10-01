e096a3f482ed2d8f0147516edec5b8c5e82ecd91
Mark Harmsen <mharmsenr@gmail.com>
2024-10-01 14:05:25 -0500

    post model

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ flask --debug run
 ## conda environment
 conda activate flaskProject
 
+## flush sockets
+when: Access to 127.0.0.1 was denied
+
+go to and flush sockets:
+chrome://net-internals/#sockets
+
 ## Theoretical basis
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    title: so.Mapped[str] = so.mapped_column(
+        sa.String(64), unique=True
+    )
+    body: so.Mapped[str] = so.mapped_column(sa.Text)
+    image_url: so.Mapped[Optional[str]] = so.mapped_column(sa.String(255))
+    views: so.Mapped[int] = so.mapped_column(sa.Integer, default=0)
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
