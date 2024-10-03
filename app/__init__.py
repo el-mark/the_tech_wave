efb7f82dfdc2dff9326caf325be5a418a762dc41
Mark Harmsen <mharmsenr@gmail.com>
2024-10-03 15:18:36 -0500

    Apply the custom CSP using Flask-Talisman

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,13 @@ app = Flask(__name__, static_folder='assets')
 # db = SQLAlchemy(app)
 
 if os.environ.get('FLASK_ENV') == 'production':
-    Talisman(app)
+    csp = {
+        'default-src': ["'self'"],
+        'style-src': ["'self'", 'https://cdn.jsdelivr.net'],
+        'script-src': ["'self'", 'https://cdn.jsdelivr.net'],
+    }
+
+    # Apply the custom CSP using Flask-Talisman
+    Talisman(app, content_security_policy=csp)
 
 from app import routes
