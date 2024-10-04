0a3c6ef2a9f08edd317b4a1f56887b7f22a522df
Mark Harmsen <mharmsenr@gmail.com>
2024-10-04 16:12:47 -0500

    fix article table with migrations

--- app/models.py
+++ app/models.py
@@ -6,12 +6,11 @@ from datetime import date
 
 class Article(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    title: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True)
+    title: so.Mapped[str] = so.mapped_column(sa.String(510), unique=True)
     body: so.Mapped[str] = so.mapped_column(sa.Text)
     image_url: so.Mapped[Optional[str]] = so.mapped_column(sa.String(255))
     source: so.Mapped[Optional[str]] = so.mapped_column(sa.String(510))
     source_name: so.Mapped[Optional[str]] = so.mapped_column(sa.String(64))
-    views: so.Mapped[int] = so.mapped_column(sa.Integer, default=0)
     created_at: so.Mapped[date] = so.mapped_column(sa.Date, default=date.today)
 
     def __repr__(self):
