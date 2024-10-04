a031ed501d68b1a2196189e5f8f741f45569488f
Mark Harmsen <mharmsenr@gmail.com>
2024-10-04 16:55:05 -0500

    fix SQLALCHEMY_DATABASE_URI for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,11 @@ if os.environ.get('FLASK_ENV') == 'production':
 
     # Talisman(app, force_https=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the SQLAlchemy object
